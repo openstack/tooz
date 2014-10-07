6fac8c9819b74f624215a57c3db25670985aad20
Joshua Harlow <harlowja@gmail.com>
2014-10-06 22:47:36 -0700

    Adjust the timeout to reflect the repeated retries
    When a acquisition retry is triggered the timeout should
    be adjusted to reflect the prior attempt instead of restarting
    from its initial timeout value.
    
    This makes it so that a acquire() request that ends up spinning
    with a timeout of say 5 seconds will not repeatly spin for an
    infinite number of 5 seconds, but instead will actually stop being
    acquired after 5 seconds is reached.
    
    Change-Id: Iff28ad972fdcec2784a868ae44beef5a2fa20b4e

--- tooz/drivers/ipc.py
+++ tooz/drivers/ipc.py
@@ -17,6 +17,7 @@
 # under the License.
 
 import hashlib
+import time
 
 import six
 import sysv_ipc
@@ -68,10 +69,13 @@ class IPCLock(locking.Lock):
         # Convert blocking argument to a valid timeout value
         if blocking is True:
             timeout = None
+            start_time = None
         elif blocking is False:
             timeout = 0
+            start_time = None
         else:
             timeout = blocking
+            start_time = time.time()
         while True:
             try:
                 self._lock = sysv_ipc.Semaphore(self.key,
@@ -88,8 +92,16 @@ class IPCLock(locking.Lock):
                     # Semaphore has been deleted in the mean time, retry from
                     # the beginning!
                     continue
+            if start_time is not None:
+                elapsed = max(0.0, time.time() - start_time)
+                if elapsed >= timeout:
+                    # Ran out of time...
+                    return False
+                adjusted_timeout = timeout - elapsed
+            else:
+                adjusted_timeout = timeout
             try:
-                self._lock.acquire(timeout=timeout)
+                self._lock.acquire(timeout=adjusted_timeout)
             except sysv_ipc.BusyError:
                 return False
             except sysv_ipc.ExistentialError:
