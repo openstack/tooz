18e31db320ffda97a8c3d81d3b89132f1fb3be93
Joshua Harlow <harlowja@gmail.com>
2016-08-13 20:21:14 -0700

    Makedirs only throws oserror, so only catch that
    The docs state this only throws OSError so we might
    as well just catch only that vs its parent class.
    
    Change-Id: I829996caf7ff256ff4949b8e1e0f27e233bab467

--- tooz/utils.py
+++ tooz/utils.py
@@ -146,7 +146,7 @@ def ensure_tree(path):
     """
     try:
         os.makedirs(path)
-    except EnvironmentError as e:
+    except OSError as e:
         if e.errno != errno.EEXIST or not os.path.isdir(path):
             raise
         return False
