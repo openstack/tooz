6d1e1b55cbd3dc3e522b6210fd5dfd861ca00fab
Julien Danjou <julien@danjou.info>
2017-06-08 10:28:22 +0200

    Simplify env list and test running
    This should make it easy to run any Python version test, e.g. Python 3.6,
    without any further change.
    
    Change-Id: I91a47b736371549e50b7c7fd60e46e0f87c357ae

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 set -x
 
-for d in $TOOZ_TEST_URLS
-do
-    TOOZ_TEST_URL=$d $*
-done
+if [ -n "$TOOZ_TEST_DRIVERS" ]
+then
+    IFS=","
+    for TOOZ_TEST_DRIVER in $TOOZ_TEST_DRIVERS
+    do
+        IFS=" "
+        TOOZ_TEST_DRIVER=(${TOOZ_TEST_DRIVER})
+        SETUP_ENV_SCRIPT="./setup-${TOOZ_TEST_DRIVER[0]}-env.sh"
+        [ -x  $SETUP_ENV_SCRIPT ] || unset SETUP_ENV_SCRIPT
+        $SETUP_ENV_SCRIPT pifpaf -e TOOZ_TEST run "${TOOZ_TEST_DRIVER[@]}" -- $*
+    done
+    unset IFS
+else
+    for d in $TOOZ_TEST_URLS
+    do
+        TOOZ_TEST_URL=$d $*
+    done
+fi
