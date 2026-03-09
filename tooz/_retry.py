3d93c5952173f5d67d12d59f000ef0e500570678
Stephen Finucane <stephenfin@redhat.com>
2026-03-09 16:00:12 +0000

    typing: Add type hints to tooz._retry
    Change-Id: I8f2ebecfc02ffed0fe9de376b436565b584c5b15
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- tooz/_retry.py
+++ tooz/_retry.py
@@ -12,19 +12,36 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+from collections.abc import Callable
+import datetime
+from typing import Any, TypeVar
+
 import tenacity
 from tenacity import stop
 from tenacity import wait
 
 
+WrappedFn = TypeVar('WrappedFn', bound=Callable[..., Any])
+
 _default_wait = wait.wait_exponential(max=1)
 
 
-def retry(stop_max_delay=None, **kwargs):
-    k = {"wait": _default_wait, "retry": tenacity.retry_never}
-    if stop_max_delay not in (True, False, None):
-        k['stop'] = stop.stop_after_delay(stop_max_delay)
-    return tenacity.retry(**k)
+# TODO(stephenfin): Remove kwargs
+def retry(
+    stop_max_delay: int | float | bool | None = None, **kwargs: Any
+) -> Callable[[WrappedFn], WrappedFn]:
+    if isinstance(stop_max_delay, (int, float, datetime.timedelta)):
+        return tenacity.retry(
+            wait=_default_wait,
+            retry=tenacity.retry_never,
+            stop=stop.stop_after_delay(stop_max_delay),
+        )
+
+    return tenacity.retry(
+        wait=_default_wait,
+        retry=tenacity.retry_never,
+    )
 
 
 TryAgain = tenacity.TryAgain
